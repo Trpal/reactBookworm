948d9e3b17f198f995675c0c3ebd350fa5fbeb0e
Trpal <jonikorpelin@gmail.com>
2018-03-06 21:00:00 +0200

    pwd change stuff

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 import DashboardPage from "./components/pages/DashboardPage";
+import ForgotPasswordPage from "./components/pages/ForgotPasswordPage";
+import ResetPasswordPage from "./components/pages/ResetPasswordPage";
 import SignupPage from "./components/pages/SignupPage";
 import ConfirmationPage from "./components/pages/ConfirmationPage";
 import UserRoute from "./components/routes/UserRoute";
@@ -12,13 +14,30 @@ import GuestRoute from "./components/routes/GuestRoute";
 const App = ({ location }) => (
 	<div className="ui container">
 		<Route location={location} path="/" exact component={HomePage} />
-		<Route location={location} path="/confirmation/:token" exact component={ConfirmationPage} />
+		<Route
+			location={location}
+			path="/confirmation/:token"
+			exact
+			component={ConfirmationPage}
+		/>
 		<GuestRoute location={location} path="/login" exact component={LoginPage} />
 		<GuestRoute
 			location={location}
 			path="/signup"
 			exact
 			component={SignupPage}
+		/>
+		<GuestRoute
+			location={location}
+			path="/forgot_password"
+			exact
+			component={ForgotPasswordPage}
+		/>
+		<GuestRoute
+			location={location}
+			path="/reset_password/:token"
+			exact
+			component={ResetPasswordPage}
 		/>
 		<UserRoute
 			location={location}
