4c84f3eb3f881114ffa7c159731593b3e7cb12d1
Trpal <jonikorpelin@gmail.com>
2018-03-08 19:51:02 +0200

    new book, book selectors to book reducers)

---
+++ src/components/pages/NewBookPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class NewBookPage extends Component {
+	state = {
+		book: null
+	};
+	render() {
+		return <div />;
+	}
+}
+
+export default NewBookPage;
