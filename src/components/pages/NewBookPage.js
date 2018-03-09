c602860a06629fccf189b47a8149aa097bb0cf85
Trpal <jonikorpelin@gmail.com>
2018-03-09 18:39:07 +0200

    Form to add a new book to your own books

--- src/components/pages/NewBookPage.js
+++ src/components/pages/NewBookPage.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
+import { Segment } from "semantic-ui-react";
+import SearchBookForm from "../forms/SearchBookForm";
+import BookForm from "../forms/BookForm";
 
 class NewBookPage extends Component {
 	state = {
 		book: null
 	};
+
+	onBookSelect = book => this.setState({ book });
+
+	addBook = () => console.log("hi");
+
 	render() {
-		return <div />;
+		return (
+			<Segment>
+				<h1>
+					Add new book to your collection
+					<SearchBookForm onBookSelect={this.onBookSelect} />
+					{this.state.book && (
+						<BookForm submit={this.addBook} book={this.state.book} />
+					)}
+				</h1>
+			</Segment>
+		);
 	}
 }
 
